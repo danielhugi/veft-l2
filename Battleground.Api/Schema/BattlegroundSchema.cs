fe309bbd085a54bd67d35adf11009ee7a6b38603
danielhugi <89972289+danielhugi@users.noreply.github.com>
2023-10-12 17:13:09 +0000

    Add files via upload

---
+++ Battleground.Api/Schema/BattlegroundSchema.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Instrumentation;
+
+namespace Battleground.Api.Schema;
+
+public class BattlegroundSchema : GraphQL.Types.Schema
+{
+    public BattlegroundSchema(IServiceProvider provider)
+        : base(provider)
+    {
+        FieldMiddleware.Use(new InstrumentFieldsMiddleware());
+    }
+}
